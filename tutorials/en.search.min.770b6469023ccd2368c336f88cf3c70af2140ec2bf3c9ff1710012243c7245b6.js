fbbb837d528a56f5149d4747aa6c239238f43a57
yosarawut <yo_sarawut@hotmail.com>
2020-01-27 00:56:18 +0700

    .

---
+++ tutorials/en.search.min.770b6469023ccd2368c336f88cf3c70af2140ec2bf3c9ff1710012243c7245b6.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/library/tutorials/flexsearch.min.js');loadScript('/library/tutorials/en.search-data.min.55b628b721bf7c8bcef40519bf013206e5ca06e037b51e279a2e5a14c123211a.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
---
+++ tutorials/en.search.min.cea1133e9b29e4d856c65970fdf9c3648003a14ab3aa47858673822782b70898.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/library/tutorials/flexsearch.min.js');loadScript('/library/tutorials/en.search-data.min.0a89a768ea7c087f8a7693d3c920a45378bec342fd5074a0badc986f268fce19.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
